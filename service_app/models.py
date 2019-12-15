49ef627258200af5a928e14a37e2aef85f659454
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-15 22:15:42 +0530

    added Status column

--- service_app/models.py
+++ service_app/models.py
@@ -10,6 +10,13 @@ class AudioDataModel(models.Model):
     error_occurred = models.BooleanField(default=False, null=False, blank=False)
     error_message = models.TextField(null=True, blank=True)
 
+    STATUS_CHOICES = (
+        ('PEN', 'Pending'),
+        ('COM', 'Complete'),
+        ('ERR', 'Error'),
+    )
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, null=False, blank=False, default='PEN')
+
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
 
