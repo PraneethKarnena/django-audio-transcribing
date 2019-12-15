6096367e9541356c519bf99a6baeaad0d57153ba
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-15 21:56:18 +0530

    created database table for Audio data

--- service_app/models.py
+++ service_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class AudioDataModel(models.Model):
+    uploaded_file = models.FileField(null=False, blank=False)
+    exported_file = models.FileField(null=True, blank=True)
+
+    transcription = models.TextField(null=True, blank=True)
+
+    error_occurred = models.BooleanField(default=False, null=False, blank=False)
+    error_message = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return f'{self.uploaded_file} - {self.created_at}'
