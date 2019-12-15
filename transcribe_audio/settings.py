6fed995c92afbe954123d953a1d8db9bcaa8bcff
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-15 21:29:18 +0530

    registered Service app

--- transcribe_audio/settings.py
+++ transcribe_audio/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'service_app.apps.ServiceAppConfig',
 ]
 
 MIDDLEWARE = [
